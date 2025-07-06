c8cfba38bfa3083ed152253adac9af616399d623
RACHID EL HACHIMI <rachidelhachimi6@gmail.com>
2025-07-06 22:46:57 +0100

    d

--- alx-movie-app/pages/index.tsx
+++ alx-movie-app/pages/index.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
   const router = useRouter();
 
   return (
-    <div className="bg-[#171D22] text-white">
+    <div className="bg-[#003967] text-white">
       <section
         className="h-screen bg-cover bg-center"
         style={{
@@ -16,7 +16,7 @@ const Home: React.FC = () => {
         <div className="bg-black bg-opacity-50 h-full flex flex-col justify-center items-center text-center">
           <h1 className="text-5xl md:text-7xl font-bold mb-8">
             Discover Your Next Favorite{" "}
-            <span className="text-[#E2D609]">Movie</span>
+            <span className="text-[#55e067]">Movie</span>
           </h1>
           <p className="text-lg md:text-2xl mb-8 max-w-2xl">
             Explore the latest blockbuster movies, critically acclaimed films,
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
         </div>
       </section>
 
-      <section className="py-16 px-8 md:px-44 bg-[#121018] text-center">
+      <section className="py-16 px-8 md:px-44 bg-[#0a0026] text-center">
         <h2 className="text-3xl md:text-5xl font-semibold mb-8">
           Join CineSeek Now!
         </h2>
